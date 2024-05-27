b8ceac652a6bfb2114e95a7108184098ae0507cb
barathraj-affinsys <barathraj@affinsys.com>
2024-05-27 12:29:12 +0530

    first commit

---
+++ ua.js
@@ -0,0 +1,54 @@
+const UA = navigator.userAgent;
+export const isWebkit =
+  /\b(iPad|iPhone|iPod)\b/.test(UA) &&
+  /WebKit/.test(UA) &&
+  !/Edge/.test(UA) &&
+  !window.MSStream;
+
+let mediaQueryUpdated = true;
+const mqL = [];
+
+function whenMediaChanges() {
+  mediaQueryUpdated = true;
+}
+
+const listenToMediaQuery = isWebkit
+  ? (mQ, f) => {
+      if (/height|width/.test(mQ.media)) {
+        mqL.push([mQ, f]);
+      }
+      mQ.addListener(f);
+      mQ.addListener(whenMediaChanges);
+    }
+  : () => {};
+
+const destroyMediaQuery = isWebkit
+  ? (mQ) => {
+      for (let i = 0; i < mqL.length; i++) {
+        if (mqL[i][0] === mQ) {
+          mqL.splice(i, 1);
+        }
+      }
+      mQ.removeListener(whenMediaChanges);
+    }
+  : listenToMediaQuery;
+
+let orientationChanged = false;
+addEventListener(
+  "orientationchange",
+  () => {
+    orientationChanged = true;
+  }
+  //   PASSIVE_LISTENER_OPTION
+);
+
+addEventListener("resize", () =>
+  setTimeout(() => {
+    if (orientationChanged && !mediaQueryUpdated) {
+      for (let i = 0; i < mqL.length; i++) {
+        mqL[i][1](mqL[i][0]);
+      }
+    }
+    mediaQueryUpdated = orientationChanged = false;
+  }, 0)
+);
